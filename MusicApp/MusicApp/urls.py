839e1182cbdaf469785ac6fd5a23cd22c44e734c
muhtedibulut <muhtedibulut@gmail.com>
2019-11-14 08:47:02 +0300

    JWT ile 'authentication' işlemi yapıldı

--- MusicApp/MusicApp/urls.py
+++ MusicApp/MusicApp/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from rest_framework import routers
 from django.conf.urls import url
 from app import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 router = routers.DefaultRouter()
@@ -29,7 +33,9 @@ router.register(r"albumstatistic", views.AlbumStatisticViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("", include("app.urls")),
-    path("app/", include("app.urls")),
+    # path("app/", include("app.urls")),
     url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     url(r"^", include(router.urls)),
+    url(r"^api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    url(r"^api/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- MusicApp/app/views.py
+++ MusicApp/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
 from .models import Musician, Album, AlbumStatistic
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import (
     MusicianSerializers,
     AlbumSerializers,
@@ -53,15 +54,18 @@ def musician_delete(request, pk, template_name="app/musicianDelete.html"):
 
 
 class MusicianViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Musician.objects.all()
     serializer_class = MusicianSerializers
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Album.objects.all()
     serializer_class = AlbumSerializers
 
 
 class AlbumStatisticViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = AlbumStatistic.objects.all()
     serializer_class = AlbumStatisticSerializers
