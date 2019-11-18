1cfa351a194dfd56856f06e45187fdc2daee8284
muhtedibulut <muhtedibulut@gmail.com>
2019-11-18 09:40:57 +0300

    Celery ile redis bağlantısı yapıldı

--- MusicApp/MusicApp/celery.py
+++ MusicApp/MusicApp/celery.py
@@ -1,22 +1,18 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MusicApp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MusicApp.settings")
 
-app = Celery('MusicApp')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("MusicApp")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    print("Request: {0!r}".format(self.request))
 
 
-"""@app.task
-def add(x, y):
-    return print(str(x + y))"""
-
-
-"""@app.task
+@app.task
 def reverse(string):
-    return string[::-1]"""
+    return string[::-1]
--- MusicApp/MusicApp/settings.py
+++ MusicApp/MusicApp/settings.py
@@ -14,12 +14,16 @@ import os
 
 # Celery settings
 
-CELERY_BROKER_URL = 'amqp://admin:admin@localhost:5672'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'  #CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_CACHE_BACKEND = 'django-cache'
+# CELERY_BROKER_URL = 'amqp://admin:admin@localhost:5672' #rabbitmq-server connection
+CELERY_BROKER_URL = (
+    "redis://localhost:6379/0"  # redis-server connection redis//localhost:6379
+)
+CELERY_RESULT_BACKEND = "django-db"  # rabbitmq-server
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379' #redis-server
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"  # CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_CACHE_BACKEND = "django-cache"
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
