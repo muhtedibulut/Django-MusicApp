a3a5423cb87cc6ba440db7e8f359cafe3cd4ede3
muhtedibulut <muhtedibulut@gmail.com>
2019-11-19 18:04:56 +0300

    posgresql ile bağlantı yapıldı

--- MusicApp/MusicApp/settings.py
+++ MusicApp/MusicApp/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = "MusicApp.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'music_app',
+            'USER': 'sekom',
+            'PASSWORD': 'sekom',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
 }
 
 
